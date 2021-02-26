7721e188de28d46f574c395c383af75ec4dd47b7
Bryan-Joe123 <75519189+Bryan-Joe123@users.noreply.github.com>
2021-02-26 19:53:48 +0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyC-0XF3IHrvAMdhsGTY0o4dwYRfpb1moGc",
+authDomain: "chater-ac341.firebaseapp.com",
+databaseURL: "https://chater-ac341-default-rtdb.firebaseio.com",
+projectId: "chater-ac341",
+storageBucket: "chater-ac341.appspot.com",
+messagingSenderId: "684422453726",
+appId: "1:684422453726:web:bb16b37348575cc87d254f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ script.js
@@ -0,0 +1,25 @@
+sendMsg();
+getMsg();
+
+function sendMsg(){
+    database = firebase.database();
+    var id=document.getElementById("input")
+    updateMsg(id.value)
+    id.value=""
+}
+
+function getMsg(){
+    database = firebase.database();
+    var msgRef=database.ref('Msg');
+    msgRef.on("value",function(data){
+        var dis=document.getElementById("displayText")
+        dis.innerHTML=data.val();
+    });
+}
+
+function updateMsg(Msg){
+    database = firebase.database();
+    database.ref('/').update({
+        Msg:Msg
+    });
+}
